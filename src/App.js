fc441c003b9bc1d82cbba02eb080e483f708cf79
douglas-cavalcante <henriquedouglas2803@gmail.com>
2021-09-13 22:03:07 -0300

    adiciona exemplos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { Pie } from 'react-chartjs-2';
+
+import api from './services/api';
+
+const data = {
+  labels: ['React', 'React-native', 'Node', 'Java'],
+
+  datasets: [
+    {
+      label: '# of downloads',
+      data: [1000, 600, 500, 50],
+      backgroundColor: [
+        'blue',
+        'tomato',
+        'green',
+        'red'
+      ],
+      borderWidth: 1,
+    }
+  ]
+}
 
 function App() {
+
+  const [optionSelected, setOptionSelected] = useState('')
+
+  const [options, setOptions] = useState([])
+
+  const handleGetOptionsSectors = async () => {
+    try {
+      const response = await api.get('/sectors');
+
+      const optionsFormatted = response.data.map(item => {
+        return {
+          label: item.name,
+          value: item.name
+        }
+      });
+
+      setOptions(optionsFormatted);
+    } catch (error) {
+    }
+  }
+
+
+  useEffect(() => {
+    handleGetOptionsSectors();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+
+        <div>
+          <Pie
+            data={data}
+            width={400}
+            height={350}
+            options={{ maintainAspectRatio: false }}
+            redraw={false}
+          />
+        </div>
+
+        <select value={optionSelected} onChange={(event) => setOptionSelected(event.target.value)}>
+          {options.map(option => <option key={option.value} value={option.value}>{option.label}</option>)}
+        </select>
+
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Select/index.js
@@ -0,0 +1,9 @@
+const Select = ({ options, value, onChange }) => {
+  return (
+    <select value={value} onChange={onChange}>
+      {options.map(option => <option key={option.value} value={option.value}>{option.label}</option>)}
+    </select>
+  )
+}
+
+export default Select;
